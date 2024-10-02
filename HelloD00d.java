91c305a942760ed787f57d0322ff49994a049def
Caden <binderca@cwu.edu>
2024-10-02 12:47:45 -0700

    add first version of HelloD00d.java

---
+++ HelloD00d.java
@@ -0,0 +1,17 @@
+//Name: Caden Binder 
+//Date: 05/16/2024 
+//File: HelloD00d.java
+
+//Adds java classes 
+import java.util.*;
+
+//A class that defines an object of type HelloD00d
+public class HelloD00d {
+
+   //The main method
+   public static void main(String[] args) { 
+   
+      System.out.println("Hey d00d!"); 
+   
+   }
+} 
